1a85f133c303310144e38902e9263a500bc3130c
Bipul Shahi <bipulshahi25@gmail.com>
2019-07-12 14:07:45 +0530

    Add files via upload

---
+++ Face_Recognition+Image.py
@@ -0,0 +1,45 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import sys
+
+# Get user supplied values
+imagePath = 'E:/ML_Codes/FaceDetect-master(1)/FaceDetect-master/image.png'
+cascPath = "E:/ML_Codes/FaceDetect-master(1)/FaceDetect-master/haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    image,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.CASCADE_SCALE_IMAGE
+)
+
+print("No. of Faces ", len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
